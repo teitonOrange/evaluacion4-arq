a7678e7fd2f55218f28cbe04db93ce1c45fc7cb9
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-11-20 23:39:18 -0300

    fix: bugfixes

--- src/users/DTO/create-user.dto.ts
+++ src/users/DTO/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, Max, IsUUID, max, IsBoolean } from "class-validator";
+import { IsString, IsNotEmpty, Max, IsUUID, max, IsBoolean, MaxLength } from "class-validator";
 
 export class CreateUserDTO {
 
@@ -6,28 +6,29 @@ export class CreateUserDTO {
 
     @IsString()
     @IsNotEmpty()
-    @Max(15)
+    @MaxLength(15)
     name: string;
 
     @IsString()
     @IsNotEmpty()
-    @Max(100)
+    @MaxLength(100)
     email: string;
 
     @IsString()
     @IsNotEmpty()
-    @Max(100)
+    @MaxLength(100)
     apellidos: string;
 
     @IsString()
     @IsNotEmpty()
-    @Max(30)
+    @MaxLength(30)
     password: string;
 
     @IsBoolean()
     @IsNotEmpty()
     isDeleted: boolean;
 
+   
 
     
 }
