be84600431896b8aaf3eff16ac2cfedc247aff3c
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-11-20 23:43:15 -0300

    feat: add createUser with http response
    it has logic to manage all posible hhtp responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,49 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UserDto } from './DTO/user.dto';
+import { CreateUserResponse } from './DTO/create-user-response.dto';
 
 @Controller('users')
 export class UsersController {
 
-    constructor(private usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
+    // @Post()
+    // createUser(@Body() dto: CreateUserDTO) : Promise<UserDto> {
+    //     return this.usersService.createUser(dto);
+    // }
+    @Post()
+    @HttpCode(HttpStatus.CREATED) // Código 201 por defecto para POST exitoso
+    async createUser(@Body() dto: CreateUserDTO): Promise<CreateUserResponse>{
+        try {
+        // Llamar al servicio para crear un usuario
+        const newUser = await this.usersService.createUser(dto);
+        return { statusCode: HttpStatus.CREATED, message: 'Usuario creado con éxito', data: newUser };
+      } catch (error) {
+        // Manejo de errores (400 y 500)
+        if (error.code === 'P2002') { // Prisma: Error por duplicado de clave única
+          throw new HttpException(
+            { statusCode: HttpStatus.BAD_REQUEST, message: 'Correo ya está registrado' },
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+  
+        // Otro error inesperado (500)
+        throw new HttpException(
+          { statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: 'Error interno del servidor' },
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
 
-    
+    @Get()
+    async getAllUsers(
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 10
+    ) 
+    {
+        return this.usersService.getAllUsers(page, limit);
+
+    }
 }
