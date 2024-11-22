a4fc64409c95102f640d36646b9d116b535d4af3
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-11-22 00:10:55 -0300

    feat: add global guard validations

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
     forbidNonWhitelisted: true
   }));
 
-  app.useGlobalGuards(new AuthGuard(new AuthService(new JwtService())));
+  app.useGlobalGuards(new AuthGuard(new AuthService(new JwtService(), new ConfigService())));
 
   await app.listen(process.env.PORT ?? 3000);
 }
