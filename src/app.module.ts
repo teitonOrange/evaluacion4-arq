b9a5dbae93b4f769a9e7047c3236896e9b219128
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-11-22 00:11:14 -0300

    feat: add jwt validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+  })],
 })
 export class AppModule {}
