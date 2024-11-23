494b5150e91a452693d4f7150d1d57652375f331
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-11-22 23:04:26 -0300

    feat: encripting dependencies and seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { faker } from '@faker-js/faker';
+import * as bcrypt from 'bcrypt';
+
 
 async function seed() {
     const prisma = new PrismaClient();
    
     const usersToCreate = 75;
+    const saltRounds = 10;
+
     for (let i = 0; i < usersToCreate; i++) {
+        const hashedPassword = await bcrypt.hash(faker.internet.password(), saltRounds);
+
          await prisma.user.create({
             data: {
                 name: faker.person.firstName(),
                 email: faker.internet.email(),
                 apellidos: faker.person.lastName(),
-                password: faker.internet.password(),
+                password: hashedPassword,
                 isDeleted: i % 12 === 0, 
             }
         });
